2caa8790c5fe63dc087fca3102a2a45b9d0d7e6a
hs-nazuna <h-suzuki@ist.hokudai.ac.jp>
2015-11-12 03:25:56 +0900

    add Treap.hpp

---
+++ cpp/DataStructure/Treap.hpp
@@ -0,0 +1,108 @@
+#ifndef TSUKASA_DIARY_S_TEMPLATE
+#include "../template.h"
+#endif
+
+template< typename T >
+class Treap {
+private:
+	unsigned int rnd() {
+		static unsigned int x = 123456789, y = 362436069, z = 521288629, w = 88675123;
+		unsigned int t = (x ^ (x << 11));
+		x = y; y = z; z = w;
+		return (w = (w ^ (w >> 19)) ^ (t ^ (t >> 8)));
+	}
+	
+	struct Node {
+		T val, data;
+		Node *lch, *rch;
+		int pri, cnt;
+		
+		Node(T v, int p) : val(v), data(v), pri(p), cnt(1) { lch = rch = nullptr; }
+	};
+	
+	int count(Node* t) { return t ? t->cnt : 0; }
+	T data(Node* t) { return t ? t->data : T(iINF); }
+	
+	Node *root;
+	
+	Node* update(Node *t) {
+		if (!t) return t;
+		t->cnt = count(t->lch) + count(t->rch) + 1;
+		t->data = min(min(data(t->lch), data(t->rch)), t->val);
+		return t;
+	}
+	
+	Node* merge(Node* l, Node* r) {
+		if (!l || !r) return l ? l : r;
+	
+		if (l->pri > r->pri) {
+			l->rch = merge(l->rch, r);
+			return update(l);
+		} else {
+			r->lch = merge(l, r->lch);
+			return update(r);
+		}
+	}
+	
+	typedef pair< Node*, Node* > pnn;
+	
+	pnn split(Node* t, int k) {
+		if (!t) return pnn(nullptr, nullptr);
+		
+		if (k <= count(t->lch)) {
+			pnn s = split(t->lch, k);
+			t->lch = s.second;
+			return pnn(s.first, update(t));
+		} else {
+			pnn s = split(t->rch, k - count(t->lch) - 1);
+			t->rch = s.first;
+			return pnn(update(t), s.second);
+		}
+	}
+	
+	Node* insert(Node* t, int k, T val, int pri) {
+		pnn s = split(t, k);
+		t = merge(s.first, new Node(val, pri));
+		t = merge(t, s.second);
+		return update(t);
+	}
+	
+	Node* erase(Node* t, int k) {
+		pnn s1 = split(t, k + 1);
+		pnn s2 = split(s1.first, k);
+		t = merge(s2.first, s1.second);
+		delete s2.second;
+		return update(t);
+	}
+	
+	Node* find(Node* t, int k) {
+		int c = count(t->lch);
+		if (k < c) return find(t->lch, k);
+		if (k > c) return find(t->rch, k - c - 1);
+		return t;
+	}
+	
+	void dump(Node* t) {
+		if (!t) return;
+		cout << "(";
+		dump(t->lch);
+		cout << t->val;
+		dump(t->rch);
+		cout << ")";
+	}
+	
+public:
+	Treap() : root(nullptr) {}
+	
+	void insert(int k, T val) { root = insert(root, k, val, rnd()); }
+	void erase(int k) { root = erase(root, k); }
+	Node* find(int k) { return find(root, k); }
+	int size() { return count(root); }
+	
+	void dump() {
+		dump(root);
+		cout << endl;
+	}
+	
+	~Treap() { while (root) erase(0); }
+};
