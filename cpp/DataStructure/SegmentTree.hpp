13c224e9ae5d904f8a310449b2f29ae447adb43c
hs-nazuna <h-suzuki@ist.hokudai.ac.jp>
2016-02-21 03:39:40 +0900

    modify SegmentTree.hpp

--- cpp/DataStructure/SegmentTree.hpp
+++ cpp/DataStructure/SegmentTree.hpp
@@ -13,6 +13,14 @@ private:
 	inline int right_t(int k) { return (k << 1) + 2; }
 	inline int center(int l, int r) { return (l + r) >> 1; }
 	
+	DATA calc(DATA d1, DATA d2) { return min(d1, d2); }
+	
+public:
+	SegmentTree(int n, DATA ini) {
+		for (size__ = 1; size__ < n; size__ <<= 1);
+		data.assign(2 * size__ - 1, ini);
+	}
+	
 	void update(int k, DATA a) {
 		k += size__ - 1;
 		data[k] = a;
@@ -29,14 +37,6 @@ private:
 		return calc(query(a, b, left_t(k), l, center(l, r)),
 					query(a, b, right_t(k), center(l, r), r));
 	}
-	
-public:
-	SegmentTree(int n, DATA ini) {
-		for (size__ = 1; size__ < n; size__ <<= 1);
-		data.assign(2 * size__ - 1, ini);
-	}
-	
-	DATA calc(DATA d1, DATA d2) { return min(d1, d2); }
 	
 	DATA query(int a, int b) { return query(a, b, 0, 0, size__); }
 	
